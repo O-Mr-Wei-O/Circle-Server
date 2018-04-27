447c454f7d93049b6b5e9fa2ca66f76fa88763b4
O-Mr-Wei-O <Sinpo96@163.com>
2018-04-28 01:52:01 +0800

    ①在注册时自动生成部分个人信息 ②getPersonalInfo、/api/uploadAvatar/:email、/api/updatePersonalInfo/:type三个接口

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var db = require('../config/db');
 var captcha = require('../config/sendCaptcha');
+const formidable = require('formidable');
 
 
 var co = require('co');
@@ -53,6 +54,7 @@ router.post('/api/validate', function (req, res) {
 
 // 发送验证码
 router.post('/api/captcha', function (req, res) {
+    // 阿里云只能进行国内的推送，不然显示连接超时
     if (req.body.data) {
         const email = req.body.data;
         // 生成随机6位验证码
@@ -66,16 +68,25 @@ router.post('/api/captcha', function (req, res) {
 // 注册
 router.post('/api/register', function (req, res) {
     if (req.body.data) {
-        console.log(req.body.data);
+        // console.log(req.body.data);
         const email = req.body.data.email;
         const password = req.body.data.password;
         const nickname = req.body.data.nickname;
+        //用户表注册
         db.query('insert into user(email,password,nickname) values(\'' + email + '\',\'' + password + '\',\'' + nickname + '\')', function (err, rows) {
             if (err) {
-                // console.error(err);
+                console.error(err);
                 res.json('fail');
             } else {
                 // console.log(rows);
+                // 生成个人信息（在表personalInfo里面）
+                db.query('insert into personalInfo(avatar,nickname,sex,birthday,email) values(null,\'' + nickname + '\',1,null,\'' + email + '\')', function (err, rows) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        // console.log(rows);
+                    }
+                });
                 res.json('success');
             }
         });
@@ -111,7 +122,7 @@ router.post('/api/login', function (req, res) {
                                     // admin中1表示是管理员账户，0表示不是管理员
                                     admin: 1
                                 });
-                            }else if (rows.length == 0) {
+                            } else if (rows.length == 0) {
                                 res.json({
                                     login: 'loginSuccess',
                                     nickname: rowsObject[0].nickname,
@@ -133,20 +144,101 @@ router.post('/api/login', function (req, res) {
     // res.end();
 });
 
-// 上传头像
-router.post('/api/upload',function (req,res) {
-    co(function* () {
-        // use 'chunked encoding'
-        var stream = fs.createReadStream('C:\\Users\\bo\\Desktop\\需求.txt');
-        var result = yield client.putStream('需求.txt', stream);
-        // console.log(result);
-        if (result.res.status == 200){
-            // 将外链地址返回给前台
-            res.json(result.res.requestUrls[0]);
-        }
-    }).catch(function (err) {
-        console.error(err);
-    });
+router.post('/api/getPersonalInfo', function (req, res) {
+    if (req.body.data) {
+        const email = req.body.data;
+        db.query('select * from personalinfo where email = \'' + email + '\'', function (err, rows) {
+            // console.log( rows);
+            if (rows.length != 0) {
+                res.json({
+                    avatar: rows[0].avatar,
+                    nickname: rows[0].nickname,
+                    sex: rows[0].sex,
+                    birthday: rows[0].birthday
+                });
+            } else {
+                res.json({});
+            }
+        });
+    }
 });
 
+
+// 上传头像
+router.post('/api/uploadAvatar/:email', function (req, res) {
+    // console.log(req.params.email);
+    const email = req.params.email;
+    // 跨域
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+
+    let form = new formidable.IncomingForm();
+    form.encoding = 'utf-8'; // 编码
+    form.keepExtensions = true; // 保留扩展名
+    form.maxFieldsSize = 2 * 1024 * 1024; // 文件大小
+    form.uploadDir = './public/images';  // 存储路径
+    form.parse(req, function (err, fileds, files) { // 解析 formData数据
+        if (err) {
+            return console.log(err)
+        }
+        // console.log(files);
+        // 这里的avatar是前台的组件里name
+        let imgPath = files.avatar.path; // 获取文件路径
+        let imgName = files.avatar.name; // 文件原名
+        // oss上传文件开始
+        co(function* () {
+            var stream = fs.createReadStream(imgPath);
+            var result = yield client.putStream(imgName, stream);
+            // console.log(result);
+            if (result.res.status == 200) {
+                // 将外链地址存入与email对应的数据库
+                // console.log(result.res.requestUrls[0]);
+                let sql = "update personalinfo set avatar = '" + result.res.requestUrls[0] + "' where email = '" + email + "'";
+                // console.log(sql);
+                db.query(sql, function (err, rows) {
+                    if (err) {
+                        res.send("修改头像失败 " + err);
+                    } else {
+                        res.end();
+                    }
+                });
+            }
+        });
+        // 上传结束
+        fs.unlink(imgPath, function () {
+        });// 删除文件，不保存
+    })
+});
+
+
+//修改个人信息
+router.post('/api/updatePersonalInfo/:type', function (req, res) {
+    // console.log(req.body.data);
+    // 种类
+    const type = req.params.type;
+    // 值
+    const value = req.body.data;
+    //email
+    const email = req.body.email;
+    let sql = "update personalinfo set " + type + "= '" + value + "' where email = '" + email + "'";
+    // console.log(sql);
+    db.query(sql, function (err, rows) {
+        if (err) {
+            res.send("修改个人信息失败 " + err);
+        } else {
+            if (type == 'nickname') {
+                let sql = "update user set nickname = '" + value + "' where email = '" + email + "'";
+                // console.log(sql);
+                db.query(sql, function (err, rows) {
+                    if (err) {
+                        res.send("修改nickname失败 " + err);
+                    }
+                });
+            }
+            res.end();
+        }
+    });
+
+});
 module.exports = router;
